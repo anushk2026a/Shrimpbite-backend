12b83d8532d77212bedc1b12dab4788d1a675c01
anushk2026a <anushk2026a@gmail.com>
2026-03-06 17:20:29 +0530

    razorpay setup

--- server.js
+++ server.js
@@ -3,9 +3,14 @@ dotenv.config()
 
 import app from "./app.js"
 import connectDB from "./config/db.js"
+import paymentRoutes from "./routes/paymentRoutes.js";
+
 
 // Connect DB
 await connectDB()
+
+// Register routes
+app.use("/api/payment", paymentRoutes)
 
 // Start Server
 const PORT = process.env.PORT || 5000
