36958f7ffd84f0ed61d6b893d136aba27f4bda2a
anushk2026a <anushk2026a@gmail.com>
2026-03-21 13:03:46 +0530

    fix: updated review user reference and cleaned up retailer review UI

--- models/Review.js
+++ models/Review.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const ReviewSchema = new mongoose.Schema({
     user: {
         type: mongoose.Schema.Types.ObjectId,
-        ref: "User",
+        ref: "AppUser",
         required: true
     },
     product: {
